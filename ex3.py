404ab07da2533304f37e91bc5026bd053d6410ee
scottpc <scottcarroll@admins-mbp-3.home>
2013-10-23 11:35:44 -0700

    Python Scripts
    N/A

---
+++ ex3.py
@@ -0,0 +1,37 @@
+# This prints a sentence
+print "I will now count my chickens:"
+
+# This prints "hens" and calculates 25 + 30 / 6
+print "Hens", 25.0 + 30.0 / 6.0
+# This prints roosters and calculates 100 - 25 * 3 % 4
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+# This prints a sentence
+print "Now I will count the eggs:"
+
+# This prints a calculation
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# This prints a sentence
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# This calculates something
+print 3.0 + 2.0 < 5.0 - 7.0
+
+# This prints a sentence followed by a calculation
+print "What is 3 + 2?", 3.0 + 2.0
+# This prints a sentence followed by a calculation
+print "What is 5 - 7?", 5.0 - 7.0
+
+# This prints a sentence.
+print "Oh, that's why it's False."
+
+# This prints a sentence
+print "How about some more."
+
+# This prints a sentence followed by a calculation
+print "Is it greater?", 5.0 > -2.0
+# This prints a sentence followed by a calculation
+print "Is it greater or equal?", 5.0 >= -2.0
+# This prints a sentence followed by a calculation
+print "Is it less or equal?", 5.0 <= -2.0
