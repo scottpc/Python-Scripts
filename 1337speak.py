a67b6b81eac2c3dc3826e131c0291958b65670f3
scottpc <scottcarroll@admins-mbp-3.home>
2013-10-23 13:06:54 -0700

    1337speak
    N/A

---
+++ 1337speak.py
@@ -0,0 +1,35 @@
+leet_a = "4"
+leet_b = "[3"
+leet_c = "("
+leet_d = "[)"
+leet_e = "3"
+leet_f = "F"
+leet_g = "G"
+leet_h = "|-|"
+leet_i = "1"
+leet_j = "J"
+leet_k = "|<"
+leet_l = "1"
+leet_m = "|\/|"
+leet_n = "|\|"
+leet_o = "0"
+leet_p = "P"
+leet_q = "Q"
+leet_r = "R"
+leet_s = "5"
+leet_t = "7"
+leet_u = "|_|"
+leet_v = "\/"
+leet_w = "\/\/"
+leet_x = "X"
+leet_y = "Y"
+leet_z = "Z"
+
+print "Welcome to %s%s%s%sspeak" % (
+	leet_l, leet_e, leet_e, leet_t)
+	
+print "Scott Carroll = %s%s%s%s%s %s%s%s%s%s%s%s" % (
+	leet_s, leet_c, leet_o, leet_t, leet_t, leet_c, leet_a, leet_r, leet_r, leet_o, leet_l, leet_l)
+	
+print "Blackbeard = %s%s%s%s%s%s%s%s%s%s" % (
+	leet_b, leet_l, leet_a, leet_c, leet_k, leet_b, leet_e, leet_a, leet_r, leet_d)
